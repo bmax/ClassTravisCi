c9803bbe09a8ec73f5c31b169d72f36d7efb5a64
Brandon Max <bmax1337@gmail.com>
2021-02-18 21:26:58 -0500

    Cahnging example 1

--- example.js
+++ example.js
@@ -1,5 +1,5 @@
 var num1 = parseInt(prompt('Give me a number!'))
-var num2 = parseInt(prompt('Give me another number!'))
+var num_2 = parseInt(prompt('Give me another number!'))
 
 var operation = prompt(
   'What would you like to do? (add, subtract, multiply, divide)'
